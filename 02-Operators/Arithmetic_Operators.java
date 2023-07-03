71932440262110c56607122b49e7e58799fb7c9e
PRIYUSH KHOBRAGADE <124695270+Priyush02K@users.noreply.github.com>
2023-07-03 23:07:33 +0530

    Add files via upload

---
+++ 02-Operators/Arithmetic_Operators.java
@@ -0,0 +1,7 @@
+ class Arithmetic_Operators {
+    int A=10;
+    int b=5;
+
+System.out.println("ADD :" +(A+B));
+
+} 
---
+++ 02-Operators/Relatonal.java
@@ -0,0 +1,16 @@
+public class Relatonal {
+    public static void main(String[] args) {
+        int A=10;
+        int B=9;
+        System.out.println(A==B);
+        System.out.println(A!=B);
+        System.out.println(A>B);
+        System.out.println(A<B);
+        System.out.println(A>=B);
+        System.out.println(A<=B);
+
+
+
+
+}
+}
