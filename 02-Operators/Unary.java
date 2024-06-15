1a12c29cce37db5612ebd70e198a5481068ed077
PRIYUSH KHOBRAGADE <124695270+Priyush02K@users.noreply.github.com>
2024-06-16 00:03:04 +0530

    Add files via upload

---
+++ 02-Operators/Unary.java
@@ -0,0 +1,28 @@
+public class Unary {
+
+    public static void main(String[] args) {
+        // int a=10;
+        // int b=++a;
+        // System.out.println(a);
+        // System.out.println(b);//pre increment
+
+        // int a=10;
+        // int b=a++;
+        // System.out.println(a);
+        // System.out.println(b);//post increment
+
+
+        // int a=10;
+        // int b=--a;
+        // System.out.println(a);
+        // System.out.println(b);//pre decrement
+
+
+        int a=10;
+        int b=a--;
+        System.out.println(a);
+        System.out.println(b);//pre increment
+
+
+    }
+}
